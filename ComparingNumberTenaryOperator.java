d1d955e8a0ffba5e072f39588e2bc360180ef464
Geroge S. Mulbah <gsmulbah2500@gmail.com>
2024-12-25 13:45:44 +0000

    added the comparing Tenary Operator

---
+++ ComparingNumberTenaryOperator.java
@@ -0,0 +1,8 @@
+class ComparingNumberTenaryOperator {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int result = (a > b) ? a : b;
+    System.out.println("Result: " + result);
+  }
+}
