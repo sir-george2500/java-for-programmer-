acae473f868009e450694370cf3544824d741da6
Geroge S. Mulbah <gsmulbah2500@gmail.com>
2024-12-25 13:31:39 +0000

    demoing java

---
+++ Demo.java
@@ -0,0 +1,17 @@
+class Demo {
+  public static void main(String[] args) {
+    int a = 10;
+    double b = 20.5;
+    float c = 30.5f;
+    long i = 53443;
+    char cha = 'A';
+    boolean d = true;
+    System.out.println(a + " " + "Hello, World!");
+
+    show();
+  }
+
+  public static void show() {
+    System.out.println("Some Show");
+  }
+}
