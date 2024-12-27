d61ec17f02d81dc0bde1529fbda937b073291873
Geroge S. Mulbah <gsmulbah2500@gmail.com>
2024-12-27 10:08:48 +0000

    added getter and seeter

--- OOP/AdvanceKeyboard.java
+++ OOP/AdvanceKeyboard.java
@@ -1,6 +1,25 @@
 package OOP;
 
 public class AdvanceKeyboard extends Keyboard {
+  private String lightColor;
+  private int lightIntensity;
+
+  int getLightIntensity() {
+    return lightIntensity;
+  }
+
+  void setLightIntensity(int lightIntensity) {
+    this.lightIntensity = lightIntensity;
+  }
+
+  String getLightColor() {
+    return lightColor;
+  }
+
+  void setLightColor(String lightColor) {
+    this.lightColor = lightColor;
+  }
+
   public void type() {
     System.out.println("Typing...");
   }
